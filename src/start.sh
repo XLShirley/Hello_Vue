72175c72a530b0cb68e824096680342f59931bf8
肖玲 <shirley_xiao@topsky.com>
2022-07-07 11:46:14 +0800

    修改启动文件

--- src/start.sh
+++ src/start.sh
@@ -4,10 +4,7 @@ git pull
 yarn --registry=https://registry.npm.taobao.org/ && yarn build
 
 # 删除容器
-docker rm -f Hello_vue &> /dev/null
+docker rm -f demo1 &> /dev/null
 
 # 启动容器
-docker run -d --restart=on-failure:5\
-        -p 8080:80 \
-        -v $PWD/dist:/usr/share/nginx/html \
-        --name Hello_vue nginx
+docker run -d --restart=on-failure:5 -p 8080:80 -v $PWD/dist:/usr/share/nginx/html --name Hello_vue nginx
